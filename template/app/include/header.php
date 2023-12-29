cb116ae4563708ab68052d9798bf6094cd365b97
sarasov matvey <sarasovmatvej@gmail.com>
2023-12-29 12:27:26 +0600

    change template history

--- template/app/include/header.php
+++ template/app/include/header.php
@@ -76,3 +76,16 @@
 
 </head>
 
+<?php
+//  Уберу фон со служебных страниц и с главной
+$pages_url = ["index","o-kompanii", "rassrochka", "garantii", "portfolio", "oplata-i-vozvrat", "skidki", "dostavka", "vakansii", "kontakty", "faq"];
+if (in_array($XNOW["page"], $pages_url)){
+    ?>
+        <body class="none_bg">
+    <?
+}else{
+    ?>
+        <body>
+    <?
+    }
+?>
